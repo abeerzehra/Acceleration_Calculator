b0c57335b472b38c97a52f1329a5852b96e1959f
abeerzehra <k164068@nu.edu.pk>
2020-04-03 12:58:05 +0500

    First Commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>calculator </title>
+<link rel="stylesheet" href="StyleSheet.css">
+
+</head>
+<body>
+   
+    <div class="fixed-header">
+        
+    </div>
+
+    
+
+</body>
+
+
+</html>
